92131a35a1fd9f2b5af5fac68c027bebfe45fd76
burstlam <burstlam@gmail.com>
2012-06-17 16:40:50 +0800

    Add ZTE Blade and AllWinner A10 to vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
+add_lunch_combo cm_a10-userdebug
 add_lunch_combo cm_anzu-userdebug
+add_lunch_combo cm_blade-userdebug
 add_lunch_combo cm_captivatemtd-userdebug
 add_lunch_combo cm_coconut-userdebug
 add_lunch_combo cm_crespo-userdebug
