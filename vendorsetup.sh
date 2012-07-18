e89b2f0d4a8910984fc87648e65f574b3735aa27
burstlam <burstlam@gmail.com>
2012-07-18 21:15:46 +0800

    Add Blade to lunch menu

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 #add_lunch_combo cm_anzu-userdebug
+add_lunch_combo cm_blade-userdebug
 #add_lunch_combo cm_captivatemtd-userdebug
 #add_lunch_combo cm_coconut-userdebug
 #add_lunch_combo cm_crespo-userdebug
