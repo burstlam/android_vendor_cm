4c0b9cc3061242537a1119e3eaabd10074659b69
burstlam <burstlam@gmail.com>
2013-12-11 01:48:31 +0800

    Add prebuilt Phone Location libs

--- config/common.mk
+++ config/common.mk
@@ -107,6 +107,10 @@ PRODUCT_COPY_FILES +=  \
     vendor/cm/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/cm/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
+# Copy phoneloc files
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/media/mokee-phoneloc.dat:system/media/mokee-phoneloc.dat
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
